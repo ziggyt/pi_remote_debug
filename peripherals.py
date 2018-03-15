f047b3d22b1601496394ed97caba39777c1847a5
ziggyt <33518806+ziggyt@users.noreply.github.com>
2018-03-15 21:04:30 +0100

    Add files via upload

--- peripherals.py
+++ peripherals.py
@@ -6,9 +6,17 @@ from time import sleep as delay
 display = lcd_i2c_driver.lcd()
 
 
+
 def handleInterrupt(channel):
+    countdown = 0
     display.lcd_clear()
     display.lcd_display_string("Shutting down", 1)
+
+    for i in range(5):
+        display.lcd_display_string(str(countdown), 2)
+        countdown += countdown + 1
+        delay(1)
+
     delay(1)
     display.lcd_clear()
     GPIO.cleanup()
